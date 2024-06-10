8e08f27e88e39544b2e9aead4398f1335081b921
kaden103 <141582493+kaden103@users.noreply.github.com>
2024-06-10 01:42:56 -0500

    Add files via upload
    This program processes a CSV file containing Walmart’s inventory data. It categorizes each item under its respective primary category and computes the aggregate monetary value for the items within each category. The result is an organized summary that presents the total value of goods for each primary category at Walmart.

---
+++ WalmartCSV (1).py
@@ -0,0 +1,35 @@
+import csv
+import locale
+locale.setlocale(locale.LC_ALL, '')
+# Introuduce the file and open to begin unpacking the data inside
+FILENAME = 'walmart_inventory.csv'
+#Opening the CSV file to store inside the fidleHdl variable
+fileHdl = open(FILENAME, 'r', encoding='utf-8-sig')
+# Use the CSV function to read the CSV FILE type into the dictRdr variable
+dictRdr = csv.DictReader(fileHdl)
+# Create inventoryCost dictionary to store obtained data
+inventoryCost = dict()
+
+def main(): # Main function which ties functions together, and creates the apps headings "Primary" and "Values"
+    CostCollector()
+    print(f"{'Primary Category' : >58}", f"{'Values' : ^52}")
+    DisplayInventoryCost()
+
+
+
+
+def CostCollector():
+    for row in dictRdr: # Loop through the file for each row inside the Primary category section, and it's corresponding value, then interst it into inventory cost
+        if row['primary_category'] in inventoryCost.keys():
+            inventoryCost[row['primary_category']] += float(row['price']) * float(row['quantity'])
+        else:
+            inventoryCost[row['primary_category']] = float(row['price']) * float(row['quantity'])
+
+def DisplayInventoryCost(): # Function responsible for display of key and corresponding value pair
+    for key, value in inventoryCost.items():
+        print(f"{key : >60}",  f"{locale.currency(value, grouping=True) : >30}")
+
+
+
+main()
+
